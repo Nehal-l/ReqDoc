8acdc0aac69f1e6775782f00d63dd57d97b95d44
Nehal Lekkala <lekkalanehal@gmail.com>
2026-08-23 22:03:53 +0530

    Add files via upload

---
+++ fake.js
@@ -0,0 +1,11 @@
+function show(param_div_id) {
+  document.getElementById('main').innerHTML = document.getElementById(param_div_id).innerHTML;
+}
+function sho(sho_id){
+  document.getElementById('m').innerHTML = document.getElementById(sho_id).innerHTML;
+}
+function fun(){
+            
+  document.getElementById('btn_box').style.display='block';
+ 
+}
